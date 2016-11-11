fe1f89fcb768253c7423e60a64a5ec30f86c40c2
apetri <apetri@phys.columbia.edu>
2016-11-11 17:04:25 -0500

    added raytracing section

--- Figures/miscellaneous.py
+++ Figures/miscellaneous.py
@@ -15,30 +15,30 @@ def distortion(cmd_args,fontsize=22):
 	#Original image
 	b = np.arange(256.)
 	g = np.array(np.meshgrid(b,b,indexing="xy"))
-	beta = lt.ConvergenceMap(np.exp(-((g[0]-128)**2+(g[1]-128)**2)/(50.**2)),angle=3.5*u.deg)
-	theta = g*beta.resolution.to(u.deg)
+	beta = lt.ConvergenceMap(np.exp(-((g[0]-128)**2+(g[1]-128)**2)/(50.**2)),angle=5*u.arcsec)
+	theta = g*beta.resolution.to(u.arcsec)
 
 	#Original
 	beta.visualize(fig=fig,ax=ax[0,0])
 	ax[0,0].set_title("Original",fontsize=fontsize)
 
-	#Distortion2: kappa, gamma1
-	J = np.array([[0.25,0],[0,0.75]])   
+	#Distortion2: gamma1
+	J = np.array([[0.75,0],[0,1.25]])   
 	dist = lt.ConvergenceMap(beta.getValues(*transf(theta,beta.side_angle/2,J)),angle=beta.side_angle)
 	dist.visualize(fig=fig,ax=ax[0,1])
-	ax[0,1].set_title(r"$\kappa=0.5,\gamma^1=0.25$",fontsize=fontsize)
+	ax[0,1].set_title(r"$\gamma^1=0.25$",fontsize=fontsize)
 
-	#Distortion3: kappa, gamma2
-	J = np.array([[0.5,-0.25],[-0.25,0.5]])   
+	#Distortion3: gamma2
+	J = np.array([[1.0,-0.25],[-0.25,1.0]])   
 	dist = lt.ConvergenceMap(beta.getValues(*transf(theta,beta.side_angle/2,J)),angle=beta.side_angle)
 	dist.visualize(fig=fig,ax=ax[1,0])
-	ax[1,0].set_title(r"$\kappa=0.5,\gamma^2=0.25$",fontsize=fontsize)
+	ax[1,0].set_title(r"$\gamma^2=0.25$",fontsize=fontsize)
 
-	#Distortion4: kappa, gamma1, omega
-	J = np.array([[0.25,-0.25],[0.25,0.75]])   
+	#Distortion4: gamma1, omega
+	J = np.array([[0.75,-0.25],[0.25,1.25]])   
 	dist = lt.ConvergenceMap(beta.getValues(*transf(theta,beta.side_angle/2,J)),angle=beta.side_angle)
 	dist.visualize(fig=fig,ax=ax[1,1])
-	ax[1,1].set_title(r"$\kappa=0.5,\gamma^1=0.25,\omega=0.25$",fontsize=fontsize)
+	ax[1,1].set_title(r"$\gamma^1=0.25,\omega=0.25$",fontsize=fontsize)
 
 	#Save 
 	fig.savefig("{0}/distortion.{0}".format(cmd_args.type))
